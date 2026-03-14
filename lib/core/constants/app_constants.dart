ddf6f51cbb901be120cc5aca5f38ae416773112f
andre.pereira <andre.pereira@relexsolutions.com>
2026-03-14 23:25:59 +0000

    feat: update version to 2.6.0 and modify roadmap for biannual billing cycle

--- lib/core/constants/app_constants.dart
+++ lib/core/constants/app_constants.dart
@@ -4,7 +4,7 @@ import 'package:flutter_dotenv/flutter_dotenv.dart';
 abstract final class AppConstants {
   /// Current application version.
   /// This should match the version in pubspec.yaml.
-  static const String version = '2.5.2';
+  static const String version = '2.6.0';
 
   /// Application name.
   static const String appName = 'SubTracker';
